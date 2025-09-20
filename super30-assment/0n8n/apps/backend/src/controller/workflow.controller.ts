41dfae379b86a82fb7d71469fc0d0fa17412dbc6
sforsanjnarao <sanjnarao26@gmail.com>
2025-09-21 01:52:32 +0530

    feat: implement workflow creation and retrieval logic with Prisma integration

--- super30-assment/0n8n/apps/backend/src/controller/workflow.controller.ts
+++ super30-assment/0n8n/apps/backend/src/controller/workflow.controller.ts
@@ -1,9 +1,34 @@
 
 import type { Request, Response } from 'express';
+import prisma from '../lib/prisma.ts';
 
-export const workflowController=async (req:Request, res:Response) => {
-    res.send({ message: "Workflow created" });
+export const workflowController=  async (req:Request, res:Response) => {
+    const { name, nodes, connections } = req.body;
+    if (!name || !nodes || !connections) {
+        return res.status(400).send({ message: "Missing required fields" });
+    }
+
+    const createWebFlow= await prisma.WorkflowEntity.create({
+        data:{
+            name,
+            nodes,
+            connections
+        }
+    })
+    console.log(createWebFlow)
+    res.status(201).json({ message: "Workflow created successfully" , createWebFlow});
 }
 export const workflowIDController=async (req:Request, res:Response) => {
+    const { id } = req.params;
+    const allWorkFlows= await prisma.WorkflowEntity.findUnique({
+        where:{
+            id:id
+        }
+    })
+    if (!allWorkFlows) {
+        return res.status(404).json({ message: "Workflow not found" });
+    }
+    console.log(allWorkFlows)
+    await prisma.$disconnect()
     res.send({ message: "Workflow Id created" });
 }
