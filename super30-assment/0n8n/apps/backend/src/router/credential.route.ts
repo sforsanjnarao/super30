81b1fd5b7bbbd33f53d8462ce8a04da5324e45ec
sforsanjnarao <sanjnarao26@gmail.com>
2025-09-27 09:44:31 +0530

    adding GET and POST to credintial route and /workflow/:id/run as startButtonForManualTrigger and in fronted we did make some new files

---
+++ super30-assment/0n8n/apps/backend/src/router/credential.route.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+const router=Router()
+
+router.post('/credential')
+router.get('/credential')
--- super30-assment/0n8n/apps/backend/src/router/workflow.route.ts
+++ super30-assment/0n8n/apps/backend/src/router/workflow.route.ts
@@ -1,11 +1,11 @@
 import Router, { type Request, type Response } from "express";
-import { ActivateWorkflow, workflowController, workflowIDController } from "../controller/workflow.controller.ts";
+import { ActivateWorkflow, startButtonForManualTrigger, workflowController, workflowIDController } from "../controller/workflow.controller.ts";
 import { protect } from "../middleware/routesProtect.ts";
 const router = Router();
 router.post("/workflows",protect, workflowController)
 router.get("/workflows/:id",protect, workflowIDController)
 router.put("/:id/activated",protect,ActivateWorkflow )
-
+router.post("/workflow/:id/run",protect,startButtonForManualTrigger)
 
 
 
