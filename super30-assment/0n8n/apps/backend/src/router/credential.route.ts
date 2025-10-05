79d1b2ccb9055d85d071726867b17b013182bf0b
sforsanjnarao <sanjnarao26@gmail.com>
2025-10-05 19:11:31 +0530

    credentials routes are done just need to done with logics

--- super30-assment/0n8n/apps/backend/src/router/credential.route.ts
+++ super30-assment/0n8n/apps/backend/src/router/credential.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { getCredential, getCredentialOfPercrulID, postCredential } from "../controller/credential.controller.ts";
 const router=Router()
 
-router.post('/')
-router.get('/')
-
+router.post('/',postCredential)
+router.get('/',getCredential )
+router.get('/:id',getCredentialOfPercrulID)
 export default router
